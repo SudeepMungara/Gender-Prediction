80a8078890fbe459f29a94a695a6a0bac552378d
SudeepMungara <sudeepmungara27@gmail.com>
2023-09-28 12:18:43 -0400

    updated file structure and added procfile

--- Frontend/app.py
+++ Frontend/app.py
@@ -8,7 +8,6 @@ import os
 st.title("Gender Predictor")
 user_input = st.text_input("Please input names (comma-seperated):")
 if st.button('Predict'):
-    os.system('py src/genderClassifyAPI.py')
     input_data = {'Names':[name for name in user_input.split(',')]}
     response = requests.post('http://127.0.0.1:5000/gender_classifier/',json=input_data,verify=False)
     if response.status_code == 200:
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: sh setup.sh  && streamlit run app.py && uvicorn src.genderClassifyAPI:app --host=0.0.0.0 --port=${PORT:-5000}
